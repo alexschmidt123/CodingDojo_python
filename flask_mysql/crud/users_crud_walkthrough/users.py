64eb4bcba6057697759c1bc847235f594842af86
Gaoming Lin <gaominglins@gmail.com>
2022-02-21 09:56:06 -0500

    p

--- flask_mysql/crud/users_crud_walkthrough/users.py
+++ flask_mysql/crud/users_crud_walkthrough/users.py
@@ -2,4 +2,11 @@ from mysqlconnection import MySQLConnection
 
 class User:
     def __init__(self,data):
-        
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.created_at = data['created_at']
+        self.updated = data['updated']
+
+    classmethod(cls,data) 
